adb001a6c8647d4f476ff8626d41ff9a761e67d8
BenCommet <bencommet@gmail.com>
2016-11-05 14:25:52 -0400

    Added classes to handle data

---
+++ src/project2/dreamteam/FileTable.java
@@ -0,0 +1,37 @@
+package project2.dreamteam;
+import java.io.Serializable;
+import java.util.Vector;
+
+/**
+ * Created by bcom3_000 on 11/5/2016.
+ */
+public class FileTable implements Serializable{
+    private Vector <FileObject> files;
+    public FileTable(){
+        this.files = new Vector<FileObject>();
+    }
+
+    public void addFile(String pName, String pDescription, String pUser){
+        FileObject newFile = new FileObject(pName, pDescription, pUser);
+        this.files.add(newFile);
+    }
+
+    public void cascadeByUsername(String pUsername){
+        for (FileObject file : this.files){
+            if(file.getUsername().equals(pUsername)){
+                this.files.remove(file);
+            }
+        }
+    }
+
+    public Vector<FileObject> searchByDesctiption(String pKeyword){
+        Vector<FileObject> matches = new Vector<FileObject>();
+        for (FileObject file : this.files){
+            if(file.getDescription().toLowerCase().contains(pKeyword.toLowerCase())){
+                matches.add(file);
+            }
+        }
+        return matches;
+    }
+
+}
---
+++ src/project2/dreamteam/UserTable.java
@@ -0,0 +1,23 @@
+package project2.dreamteam;
+
+import java.io.Serializable;
+import java.util.Vector;
+
+/**
+ * Created by bcom3_000 on 11/5/2016.
+ */
+public class UserTable implements Serializable{
+    private Vector <UserObject> users;
+    public UserTable(){
+         this.users = new Vector<UserObject>();
+    }
+
+    public void addUser(String pUsername, String pConnectionSpeed, String pHostname){
+        UserObject newUser = new UserObject(pUsername, pConnectionSpeed, pHostname);
+        this.users.add(newUser);
+    }
+
+    public boolean removeUser(String pUsername){
+        return users.remove(pUsername);
+    }
+}
